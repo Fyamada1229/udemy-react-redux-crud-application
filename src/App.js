d8efa60e3ac53415a976a299475e4f917011d908
Fyamada1229 <yamadafutosi1229@gmail.com>
2021-12-01 23:31:50 +0900

    add: propsのコード追加
    propsの基礎を振り返り

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Futoshi", age: 31 },
+    { name: "No name" },
+  ];
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      {profiles.map((profiles, index) => {
+        return <User name={profiles.name} age={profiles.age} key={index} />;
+      })}
+      <User name={"Futoshi"} age={31} />
     </div>
   );
+};
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1,
 };
 
 export default App;
