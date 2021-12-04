e3685cae24998c09d48e668b5b2aa43d7a31606a
Fyamada1229 <yamadafutosi1229@gmail.com>
2021-12-05 00:47:15 +0900

    add: tableダグ作成

--- src/components/EventsIndex.js
+++ src/components/EventsIndex.js
@@ -5,25 +5,33 @@ import { User } from "./User";
 
 class EventsIndex extends Component {
   componentDidMount() {
-    console.log("hi");
     this.props.readEvents();
   }
 
   render() {
     const props = this.props;
 
+    console.log(props.event);
     return (
       <>
         <div>
           <User name="Yamada" age={31} />
         </div>
-        <div></div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>タイトル</th>
+              <th>内容</th>
+            </tr>
+          </thead>
+        </table>
       </>
     );
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({ events: state.events });
 const mapDispatcToProps = { readEvents };
 
 // dispatchのショートハンドではない書き方。
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,14 +1,12 @@
+import _ from "lodash";
 import { READ_EVENTS } from "../action/index";
 
-// 状態の初期
-const initialState = { value: 0 };
-
 // 関数の内部で受け取ったtypeでactionに返す
-export default (state = {}, action) => {
+export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
-      return state;
+      return _.mapKeys(action.response.data, "id");
     default:
-      return state;
+      return events;
   }
 };
